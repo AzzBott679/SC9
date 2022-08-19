e7eadcad45462e9d6a9acaf149e3a40d87489286
AlexPakel <111372564+AlexPakel@users.noreply.github.com>
2022-08-19 17:27:55 +0700

    config.js
    Djudhc

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
-global.sessionName = 'Xavior'
+global.owner = ['628217143235i']
+global.premium = ['6282171432358']
+global.packname = 'MEGABOT'
+global.author = 'By Alex'
+global.sessionName = 'Xpp'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
